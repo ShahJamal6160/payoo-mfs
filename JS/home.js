f9344d3dfd02d85204543e796bbd9fc1f0cacf7b
ShahJamal6160 <shahj6160@gmail.com>
2024-09-19 00:13:05 +0600

    add money & update balance

--- JS/home.js
+++ JS/home.js
@@ -13,5 +13,26 @@ document.getElementById("btn-add-money")
         // get the number provider
         const pinNumberInput = document.getElementById('input-pin-nember').value;
         
-        console.log(addMoneyInput, pinNumberInput);
+        console.log(typeof addMoneyInput, typeof pinNumberInput);
+
+        //step-3 verfy the pin- this wrong way
+        if(pinNumberInput === '1234'){
+            console.log('adding Money to your Account');
+            //step-4 get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(typeof balance);
+
+            //step-5 add adamoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            //step-6 update the balance in thr UI
+            document.getElementById('account-balance').innerText = newBalance;
+
+        }
+        else{
+            alert('Failed to add money! plz try again')
+        }
 })
